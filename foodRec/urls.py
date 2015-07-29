6e308a6ed72117b1a8e644437e4a7d6c3c28a835
Robin Liu <renfeng.liu@gmail.com>
2015-07-29 11:05:47 -0400

    fix url problem

--- foodRec/urls.py
+++ foodRec/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$',views.fileupload_view ,name='fileupload_view'),
-    url(r'^fileupload$', views.fileupload_view ,name='fileupload_view'),
+    url(r'^fileupload/$', views.fileupload_view ,name='fileupload_view'),
     url(r'^classifyimage', views.classifyimage_view, name='classifyimage_view')
     # url(r'^$', include('AppServer.urls')),
     # url(r'^(.*)', include('AppServer.urls'))
